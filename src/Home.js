d5f31091bba605bdec3ecfbe72dc321680cae345
builtbybo <lanlecreates@gmail.com>
2019-12-29 21:30:09 +0000

    updates

--- src/Home.js
+++ src/Home.js
@@ -17,11 +17,9 @@ class Home extends Component{
                 </div>
                 <p className="lead" style={biggerLead}>
                   This simple app creates meetings, allows people to check in,
-                  and picks random users to award giveaways. It's a good example
+                  and picks random users to award giveaways. It's a working example
                   of a Single Page Application which includes connection to a
-                  database and routing. It's a practical way to learn
-                  <Link to="https://reactjs.org/"> React</Link> with
-                  <Link to="https://firebase.google.com"> Firebase</Link>.
+                  Google Firebase realtime database and routing. 
                 </p>
 
                 {user == null && (
